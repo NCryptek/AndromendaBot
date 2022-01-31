1150cee8ad48ba953d44e589b4365b4c00db9b3a
NCryptek <84776159+NCryptek@users.noreply.github.com>
2022-01-31 17:06:30 +0100

    Update funkcje.py

--- funkcje.py
+++ funkcje.py
@@ -2,8 +2,10 @@ bot = "strat"
 bot_hi = bot + ' hi'
 bot_inspire = bot + ' inspire'
 bot_help = bot + ' help'
+bot_artillery = bot + ' artillery'
+bot_shame = bot + ' shame'
 
 def get_help():
-  help = "``` {} - wita się \n {} - motywujące coś \n {} - pomoc ```".format(bot_hi, bot_inspire, bot_help)
+  help = "``` {} - wita się \n {} - motywujące coś \n {} - pomoc \n {} - ARTILLERY!```".format(bot_hi, bot_inspire, bot_help, bot_artillery)
 
   return help
