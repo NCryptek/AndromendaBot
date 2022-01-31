7041290a5733ea78def09dc0ad318f4bba766d9a
NCryptek <84776159+NCryptek@users.noreply.github.com>
2022-01-31 15:08:17 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import discord
+import requests
+import json
+
+client = discord.Client()
+
+def get_inspire():
+  
+
+@client.event
+async def on_ready():
+    print('we have logged {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+      return
+
+    if message.content.startswith('$StrategOS'):
+      await message.channel.send('Hello!')
+
+client.run(os.environ['TOKEN'])
