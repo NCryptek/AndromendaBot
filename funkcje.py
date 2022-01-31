843d4df323681e8ad492c1e84ea1aa7799868519
NCryptek <84776159+NCryptek@users.noreply.github.com>
2022-01-31 15:55:03 +0100

    Create funkcje.py

---
+++ funkcje.py
@@ -0,0 +1,9 @@
+bot = "strat"
+bot_hi = bot + ' hi'
+bot_inspire = bot + ' inspire'
+bot_help = bot + ' help'
+
+def get_help():
+  help = "``` {} - wita się \n {} - motywujące coś \n {} - pomoc ```".format(bot_hi, bot_inspire, bot_help)
+
+  return help
